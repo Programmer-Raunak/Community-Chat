fa2f4199fc4eb7b779c5646da4059baa3100787f
𝐑𝐀𝐔𝐍𝐀𝐊  ||  ᴡᴇʙ ᴅᴇᴠᴇʟᴏᴘᴇʀ <158747587+Programmer-Raunak@users.noreply.github.com>
2024-11-09 02:26:21 +0530

    Update client.js

--- public/client.js
+++ public/client.js
@@ -1,4 +1,4 @@
-const socket = io("https://community-chat-eight.vercel.app")
+const socket = io()
 let name;
 let textarea = document.querySelector('#textarea')
 let messageArea = document.querySelector('.message__area')
